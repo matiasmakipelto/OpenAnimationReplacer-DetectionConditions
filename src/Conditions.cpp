f62e815733c8039c6c8b116835af0f647984b483
Matias Mäkipelto <matias.timoaleksi@hotmail.com>
2023-11-19 13:56:56 +0900

    Added two new subconditions: distance and keyword. Also refactored the code to inherit from a parent class to avoid code duplication.

--- src/Conditions.cpp
+++ src/Conditions.cpp
@@ -7,7 +7,6 @@ namespace Conditions
 	static int32_t* g_RelationshipRankTypeIdsByIndex = (int32_t*)REL::VariantID(502260, 369311, 0x1E911A0).address();
 	using tTESNPC_GetRelationshipRankIndex = int32_t(*)(RE::TESNPC* a_npc1, RE::TESNPC* a_npc2);
 	static REL::Relocation<tTESNPC_GetRelationshipRankIndex> TESNPC_GetRelationshipRankIndex{ REL::VariantID(23624, 24076, 0x355790) };  // 345ED0, 35C270, 355790
-	
 	bool GetRelationshipRank(RE::TESNPC* a_npc1, RE::TESNPC* a_npc2, int32_t& a_outRank)
 	{
 		if (a_npc1 && a_npc2) {
@@ -17,12 +16,35 @@ namespace Conditions
 	
 		return false;
 	}
-	
 
 
 
 
-	IsDetectedByCondition::IsDetectedByCondition()
+	bool ValidateTarget(RE::Actor* actor, RE::Actor* target)
+	{
+		if (!target || !actor) {
+			return false;
+		}
+
+		if (target == actor) {
+			return false;
+		}
+
+		if (target->IsDead() || target->IsDeleted() || target->IsDisabled()) {
+			return false;
+		}
+
+		return true;
+	}
+
+
+
+	/////////////////////////////////
+	// 
+	// Base
+	// 
+	/////////////////////////////////
+	DetectionCondition::DetectionCondition()
 	{
 		// Human
 		useBoolComponent = static_cast<IBoolConditionComponent*>(AddBaseComponent(
@@ -72,15 +94,125 @@ namespace Conditions
 		actorBaseComponent = static_cast<IFormConditionComponent*>(AddBaseComponent(
 			ConditionComponentType::kForm,
 			"Actor base"));
+
+		// Distance
+		useBoolComponent5 = static_cast<IBoolConditionComponent*>(AddBaseComponent(
+			ConditionComponentType::kBool,
+			"Use the distance condition"));
+		comparisonComponent2 = static_cast<IComparisonConditionComponent*>(AddBaseComponent(
+			ConditionComponentType::kComparison,
+			"Comparison"));
+		numericComponent2 = static_cast<INumericConditionComponent*>(AddBaseComponent(
+			ConditionComponentType::kNumeric,
+			"Distance (centimeters)"));
+
+		// Keyword
+		useBoolComponent6 = static_cast<IBoolConditionComponent*>(AddBaseComponent(
+			ConditionComponentType::kBool,
+			"Use the keyword condition"));
+		notComponent3 = static_cast<IBoolConditionComponent*>(AddBaseComponent(
+			ConditionComponentType::kBool,
+			"Is NOT"));
+		keywordComponent = static_cast<IKeywordConditionComponent*>(AddBaseComponent(
+			ConditionComponentType::kKeyword,
+			"Keyword"));
 	}
 
-	bool IsDetectedByCondition::IsRelationship(RE::TESNPC* base, RE::TESNPC* target, RE::TESObjectREFR* a_refr) const
+	bool DetectionCondition::IsRelationship(RE::TESNPC* base, RE::TESNPC* target, RE::TESObjectREFR* a_refr) const
 	{
 		int32_t relationshipRank = 0;
 		GetRelationshipRank(base, target, relationshipRank);
 		return comparisonComponent->GetComparisonResult(static_cast<float>(relationshipRank), numericComponent->GetNumericValue(a_refr));
 	}
 
+	bool DetectionCondition::FitsConditions(RE::Actor* actor, RE::Actor* target, RE::TESObjectREFR* a_refr) const
+	{
+		// Humanoid
+		if (useBoolComponent->GetBoolValue()) {
+			if (isHuman->GetBoolValue() != target->HasKeywordString("ActorTypeNPC")) {
+				return false;
+			}
+		}
+
+		// Female
+		if (useBoolComponent1->GetBoolValue()) {
+			if (isFemale->GetBoolValue() != target->GetActorBase()->IsFemale()) {
+				return false;
+			}
+		}
+
+		// Relationship
+		if (useBoolComponent2->GetBoolValue()) {
+			if (!IsRelationship(actor->GetActorBase(), target->GetActorBase(), a_refr)) {
+				return false;
+			}
+		}
+
+		// Faction
+		if (useBoolComponent3->GetBoolValue() && factionComponent->IsValid()) {
+			if (const auto faction = factionComponent->GetTESFormValue()->As<RE::TESFaction>()) { // Faction was provided
+				if (notComponent->GetBoolValue() /* Read as "Must not belong to a faction"*/ == target->IsInFaction(faction))
+					return false;
+			}
+			else if (!notComponent->GetBoolValue() /* Read as "Must belong to a faction"*/) { // Faction wasn't provided
+				return false;
+			}
+		}
+
+		// ActorBase
+		if (useBoolComponent4->GetBoolValue() && actorBaseComponent->IsValid()) {
+			if (notComponent2->GetBoolValue()) { /* Shouldn't be the actor base */
+				if (target->GetActorBase() == actorBaseComponent->GetTESFormValue()) {
+					return false;
+				}
+			}
+			else { /* Should be the actor base */
+				if (target->GetActorBase() != actorBaseComponent->GetTESFormValue()) {
+					return false;
+				}
+			}
+		}
+
+		// Distance
+		if (useBoolComponent5->GetBoolValue()) {
+			const auto actorPos = actor->GetPosition();
+			const auto targetPos = target->GetPosition();
+			const auto compDistance = numericComponent2->GetNumericValue(a_refr);
+			if (comparisonComponent2->GetComparisonResult(actorPos.GetSquaredDistance(targetPos), compDistance * compDistance) == false) {
+				return false;
+			}
+		}
+
+		// Keyword
+		if (useBoolComponent6->GetBoolValue() && keywordComponent->IsValid()) {
+			if (notComponent3->GetBoolValue()) {
+				if (target->HasKeywordString(keywordComponent->GetArgument())) {
+					return false;
+				}
+			}
+			else {
+				if (!target->HasKeywordString(keywordComponent->GetArgument())) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	};
+
+
+
+
+
+
+
+
+
+	/////////////////////////////////
+	// 
+	// IsDetectedBy
+	// 
+	/////////////////////////////////
 	bool IsDetectedByCondition::EvaluateImpl([[maybe_unused]] RE::TESObjectREFR* a_refr,
 		[[maybe_unused]] RE::hkbClipGenerator* a_clipGenerator) const
 	{
@@ -93,12 +225,14 @@ namespace Conditions
 
 			if (a_actor->GetActorRuntimeData().currentProcess) {
 				if (const auto processLists = RE::ProcessLists::GetSingleton(); processLists) {
+					bool actorsExist = false;
 					for (auto& targetHandle : processLists->highActorHandles) {
 						if (const auto target = targetHandle.get(); target && target->GetActorRuntimeData().currentProcess) {
 							if (const auto base = target->GetActorBase(); base && !base->AffectsStealthMeter()) {
 								continue;
 							}
 
+							actorsExist = true;
 							if (ValidateTarget(a_actor, target.get())) {
 								if (target->RequestDetectionLevel(a_actor) > 0) {
 									if (FitsConditions(a_actor, target.get(), a_refr)) {
@@ -108,17 +242,19 @@ namespace Conditions
 							}
 						}
 					}
-				}
-			}
 
-			// The loop doesn't trigger for player. Thus, check player separately
-			const RE::FormID playerRefId = 0x00000014;
-			const auto playerRef = RE::TESForm::LookupByID(playerRefId);
-			const auto playerActor = playerRef->As<RE::Actor>();
-			if (ValidateTarget(a_actor, playerActor)) {
-				if (playerActor->RequestDetectionLevel(a_actor) > 0) {
-					if (FitsConditions(a_actor, playerActor, a_refr)) {
-						return true;
+					if (actorsExist) { // This condition exists because this function gets called even in main menu.
+						// The loop doesn't trigger for player. Thus, check player separately
+						const RE::FormID playerRefId = 0x00000014;
+						const auto playerRef = RE::TESForm::LookupByID(playerRefId);
+						const auto playerActor = playerRef->As<RE::Actor>();
+						if (ValidateTarget(a_actor, playerActor)) {
+							if (playerActor->RequestDetectionLevel(a_actor) > 0) {
+								if (FitsConditions(a_actor, playerActor, a_refr)) {
+									return true;
+								}
+							}
+						}
 					}
 				}
 			}
@@ -127,69 +263,6 @@ namespace Conditions
 		return false;
 	}
 
-	bool IsDetectedByCondition::ValidateTarget(RE::Actor* actor, RE::Actor* target) const
-	{
-		if (target == actor) {
-			return false;
-		}
-
-		if (target->IsDead() || target->IsDeleted() || target->IsDisabled()) {
-			return false;
-		}
-
-		return true;
-	}
-
-	bool IsDetectedByCondition::FitsConditions(RE::Actor* actor, RE::Actor* target, RE::TESObjectREFR* a_refr) const
-	{
-		// Humanoid
-		if (useBoolComponent->GetBoolValue()) {
-			if (isHuman->GetBoolValue() != target->HasKeywordString("ActorTypeNPC")) {
-				return false;
-			}
-		}
-
-		// Female
-		if (useBoolComponent1->GetBoolValue()) {
-			if (isFemale->GetBoolValue() != target->GetActorBase()->IsFemale()) {
-				return false;
-			}
-		}
-
-		// Relationship
-		if (useBoolComponent2->GetBoolValue()) {
-			if (!IsRelationship(actor->GetActorBase(), target->GetActorBase(), a_refr)) {
-				return false;
-			}
-		}
-
-		// Faction
-		if (useBoolComponent3->GetBoolValue() && factionComponent->IsValid()) {
-			if (const auto faction = factionComponent->GetTESFormValue()->As<RE::TESFaction>()) { // Faction was provided
-				if (notComponent->GetBoolValue() /* Read as "Must not belong to a faction"*/ == target->IsInFaction(faction))
-					return false;
-			}
-			else if (!notComponent->GetBoolValue() /* Read as "Must belong to a faction"*/) { // Faction wasn't provided
-				return false;
-			}
-		}
-
-		// ActorBase
-		if (useBoolComponent4->GetBoolValue() && actorBaseComponent->IsValid()) {
-			if (notComponent2->GetBoolValue()) { /* Shouldn't be the actor base */
-				if (target->GetActorBase() == actorBaseComponent->GetTESFormValue()) {
-					return false;
-				}
-			}
-			else { /* Should be the actor base */
-				if (target->GetActorBase() != actorBaseComponent->GetTESFormValue()) {
-					return false;
-				}
-			}
-		}
-
-		return true;
-	};
 
 
 
@@ -197,67 +270,11 @@ namespace Conditions
 
 
 
-
-
-	DetectsCondition::DetectsCondition()
-	{
-		// Human
-		useBoolComponent = static_cast<IBoolConditionComponent*>(AddBaseComponent(
-			ConditionComponentType::kBool,
-			"Use the humanoid condition"));
-		isHuman = static_cast<IBoolConditionComponent*>(AddBaseComponent(
-			ConditionComponentType::kBool,
-			"Is humanoid"));
-
-		// Gender
-		useBoolComponent1 = static_cast<IBoolConditionComponent*>(AddBaseComponent(
-			ConditionComponentType::kBool,
-			"Use the gender condition"));
-		isFemale = static_cast<IBoolConditionComponent*>(AddBaseComponent(
-			ConditionComponentType::kBool,
-			"Is Female"));
-
-		// Relationship
-		useBoolComponent2 = static_cast<IBoolConditionComponent*>(AddBaseComponent(
-			ConditionComponentType::kBool,
-			"Use the relationship condition"));
-		comparisonComponent = static_cast<IComparisonConditionComponent*>(AddBaseComponent(
-			ConditionComponentType::kComparison,
-			"Comparison"));
-		numericComponent = static_cast<INumericConditionComponent*>(AddBaseComponent(
-			ConditionComponentType::kNumeric,
-			"Relationship [-4 = Archnemesis, .., 4 = Lover]"));
-
-		// Faction
-		useBoolComponent3 = static_cast<IBoolConditionComponent*>(AddBaseComponent(
-			ConditionComponentType::kBool,
-			"Use the faction condition"));
-		notComponent = static_cast<IBoolConditionComponent*>(AddBaseComponent(
-			ConditionComponentType::kBool,
-			"Is NOT"));
-		factionComponent = static_cast<IFormConditionComponent*>(AddBaseComponent(
-			ConditionComponentType::kForm,
-			"Faction"));
-
-		// ActorBase
-		useBoolComponent4 = static_cast<IBoolConditionComponent*>(AddBaseComponent(
-			ConditionComponentType::kBool,
-			"Use the actorbase condition"));
-		notComponent2 = static_cast<IBoolConditionComponent*>(AddBaseComponent(
-			ConditionComponentType::kBool,
-			"Is NOT"));
-		actorBaseComponent = static_cast<IFormConditionComponent*>(AddBaseComponent(
-			ConditionComponentType::kForm,
-			"Actor base"));
-	}
-
-	bool DetectsCondition::IsRelationship(RE::TESNPC* base, RE::TESNPC* target, RE::TESObjectREFR* a_refr) const
-	{
-		int32_t relationshipRank = 0;
-		GetRelationshipRank(base, target, relationshipRank);
-		return comparisonComponent->GetComparisonResult(static_cast<float>(relationshipRank), numericComponent->GetNumericValue(a_refr));
-	}
-
+	/////////////////////////////////
+	// 
+	// Detects
+	// 
+	/////////////////////////////////
 	bool DetectsCondition::EvaluateImpl([[maybe_unused]] RE::TESObjectREFR* a_refr,
 		[[maybe_unused]] RE::hkbClipGenerator* a_clipGenerator) const
 	{
@@ -270,12 +287,14 @@ namespace Conditions
 
 			if (a_actor->GetActorRuntimeData().currentProcess) {
 				if (const auto processLists = RE::ProcessLists::GetSingleton(); processLists) {
+					bool actorsExist = false;
 					for (auto& targetHandle : processLists->highActorHandles) {
 						if (const auto target = targetHandle.get(); target && target->GetActorRuntimeData().currentProcess) {
 							if (const auto base = target->GetActorBase(); base && !base->AffectsStealthMeter()) {
 								continue;
 							}
 
+							actorsExist = true;
 							if (ValidateTarget(a_actor, target.get())) {
 								if (a_actor->RequestDetectionLevel(target.get()) > 0) {
 									if (FitsConditions(a_actor, target.get(), a_refr)) {
@@ -285,17 +304,19 @@ namespace Conditions
 							}
 						}
 					}
-				}
-			}
 
-			// The loop doesn't trigger for player. Thus, check player separately
-			const RE::FormID playerRefId = 0x00000014;
-			const auto playerRef = RE::TESForm::LookupByID(playerRefId);
-			const auto playerActor = playerRef->As<RE::Actor>();
-			if (ValidateTarget(a_actor, playerActor)) {
-				if (a_actor->RequestDetectionLevel(playerActor) > 0) {
-					if (FitsConditions(a_actor, playerActor, a_refr)) {
-						return true;
+					if (actorsExist) { // This condition exists because this function gets called even in main menu.
+						// The loop doesn't trigger for player. Thus, check player separately
+						const RE::FormID playerRefId = 0x00000014;
+						const auto playerRef = RE::TESForm::LookupByID(playerRefId);
+						const auto playerActor = playerRef->As<RE::Actor>();
+						if (ValidateTarget(a_actor, playerActor)) {
+							if (a_actor->RequestDetectionLevel(playerActor) > 0) {
+								if (FitsConditions(a_actor, playerActor, a_refr)) {
+									return true;
+								}
+							}
+						}
 					}
 				}
 			}
@@ -303,68 +324,4 @@ namespace Conditions
 
 		return false;
 	}
-
-	bool DetectsCondition::ValidateTarget(RE::Actor* actor, RE::Actor* target) const
-	{
-		if (target == actor) {
-			return false;
-		}
-
-		if (target->IsDead() || target->IsDeleted() || target->IsDisabled()) {
-			return false;
-		}
-
-		return true;
-	}
-
-	bool DetectsCondition::FitsConditions(RE::Actor* actor, RE::Actor* target, RE::TESObjectREFR* a_refr) const
-	{
-		// Humanoid
-		if (useBoolComponent->GetBoolValue()) {
-			if (isHuman->GetBoolValue() != target->HasKeywordString("ActorTypeNPC")) {
-				return false;
-			}
-		}
-
-		// Female
-		if (useBoolComponent1->GetBoolValue()) {
-			if (isFemale->GetBoolValue() != target->GetActorBase()->IsFemale()) {
-				return false;
-			}
-		}
-
-		// Relationship
-		if (useBoolComponent2->GetBoolValue()) {
-			if (!IsRelationship(actor->GetActorBase(), target->GetActorBase(), a_refr)) {
-				return false;
-			}
-		}
-
-		// Faction
-		if (useBoolComponent3->GetBoolValue() && factionComponent->IsValid()) {
-			if (const auto faction = factionComponent->GetTESFormValue()->As<RE::TESFaction>()) { // Faction was provided
-				if (notComponent->GetBoolValue() /* Read as "Must not belong to a faction"*/ == target->IsInFaction(faction))
-					return false;
-			}
-			else if (!notComponent->GetBoolValue() /* Read as "Must belong to a faction"*/) { // Faction wasn't provided
-				return false;
-			}
-		}
-
-		// ActorBase
-		if (useBoolComponent4->GetBoolValue() && actorBaseComponent->IsValid()) {
-			if (notComponent2->GetBoolValue()) { /* Shouldn't be the actor base */
-				if (target->GetActorBase() == actorBaseComponent->GetTESFormValue()) {
-					return false;
-				}
-			}
-			else { /* Should be the actor base */
-				if (target->GetActorBase() != actorBaseComponent->GetTESFormValue()) {
-					return false;
-				}
-			}
-		}
-
-		return true;
-	};
 }
--- src/Conditions.h
+++ src/Conditions.h
@@ -4,81 +4,70 @@
 
 namespace Conditions
 {
-	class IsDetectedByCondition : public CustomCondition
+	class DetectionCondition : public CustomCondition
+	{
+		public:
+			DetectionCondition();
+			//Human
+			IBoolConditionComponent* useBoolComponent;
+			IBoolConditionComponent* isHuman;
+			//Gender
+			IBoolConditionComponent* useBoolComponent1;
+			IBoolConditionComponent* isFemale;
+			//Relationship
+			IBoolConditionComponent* useBoolComponent2;
+			IComparisonConditionComponent* comparisonComponent;
+			INumericConditionComponent* numericComponent;
+			//Faction
+			IBoolConditionComponent* useBoolComponent3;
+			IBoolConditionComponent* notComponent;
+			IFormConditionComponent* factionComponent;
+			//ActorBase
+			IBoolConditionComponent* useBoolComponent4;
+			IBoolConditionComponent* notComponent2;
+			IFormConditionComponent* actorBaseComponent;
+			//Distance
+			IBoolConditionComponent* useBoolComponent5;
+			IComparisonConditionComponent* comparisonComponent2;
+			INumericConditionComponent* numericComponent2;
+			//Keyword
+			IBoolConditionComponent* useBoolComponent6;
+			IBoolConditionComponent* notComponent3;
+			IKeywordConditionComponent* keywordComponent;
+		protected:
+			bool IsRelationship(RE::TESNPC* base, RE::TESNPC* target, RE::TESObjectREFR* a_refr) const;
+			bool FitsConditions(RE::Actor* actor, RE::Actor* target, RE::TESObjectREFR* a_refr) const;
+	};
+
+
+	class IsDetectedByCondition : public DetectionCondition
 	{
 	public:
 		// Mandatory stuff
 		constexpr static inline std::string_view CONDITION_NAME = "IsDetectedBy"sv;
 		RE::BSString GetName() const override { return CONDITION_NAME.data(); }
 		RE::BSString GetDescription() const override { return "True if an actor that fulfills the conditions sees the actor."sv.data(); }
-		constexpr REL::Version GetRequiredVersion() const override { return { 1, 1, 0 }; }
+		constexpr REL::Version GetRequiredVersion() const override { return { 1, 2, 0 }; }
 
-
-
-		// Good stuff
-		IsDetectedByCondition();
-		//Human
-		IBoolConditionComponent* useBoolComponent;
-		IBoolConditionComponent* isHuman;
-		//Gender
-		IBoolConditionComponent* useBoolComponent1;
-		IBoolConditionComponent* isFemale;
-		//Relationship
-		IBoolConditionComponent* useBoolComponent2;
-		IComparisonConditionComponent* comparisonComponent;
-		INumericConditionComponent* numericComponent;
-		//Faction
-		IBoolConditionComponent* useBoolComponent3;
-		IBoolConditionComponent* notComponent;
-		IFormConditionComponent* factionComponent;
-		//ActorBase
-		IBoolConditionComponent* useBoolComponent4;
-		IBoolConditionComponent* notComponent2;
-		IFormConditionComponent* actorBaseComponent;
+		using DetectionCondition::DetectionCondition;
 
 	protected:
-		bool IsRelationship(RE::TESNPC* base, RE::TESNPC* target, RE::TESObjectREFR* a_refr) const;
-		bool FitsConditions(RE::Actor* actor, RE::Actor* target, RE::TESObjectREFR* a_refr) const;
-		bool ValidateTarget(RE::Actor* actor, RE::Actor* target) const;
 		bool EvaluateImpl(RE::TESObjectREFR* a_refr, RE::hkbClipGenerator* a_clipGenerator) const override;
 	};
 
 
-	class DetectsCondition : public CustomCondition
+	class DetectsCondition : public DetectionCondition
 	{
 	public:
 		// Mandatory stuff
 		constexpr static inline std::string_view CONDITION_NAME = "Detects"sv;
 		RE::BSString GetName() const override { return CONDITION_NAME.data(); }
 		RE::BSString GetDescription() const override { return "True if an actor that fulfills the conditions is visible to the actor."sv.data(); }
-		constexpr REL::Version GetRequiredVersion() const override { return { 1, 1, 0 }; }
+		constexpr REL::Version GetRequiredVersion() const override { return { 1, 2, 0 }; }
 	
-	
-	
-		// Good stuff
-		DetectsCondition();
-		//Human
-		IBoolConditionComponent* useBoolComponent;
-		IBoolConditionComponent* isHuman;
-		//Gender
-		IBoolConditionComponent* useBoolComponent1;
-		IBoolConditionComponent* isFemale;
-		//Relationship
-		IBoolConditionComponent* useBoolComponent2;
-		IComparisonConditionComponent* comparisonComponent;
-		INumericConditionComponent* numericComponent;
-		//Faction
-		IBoolConditionComponent* useBoolComponent3;
-		IBoolConditionComponent* notComponent;
-		IFormConditionComponent* factionComponent;
-		//ActorBase
-		IBoolConditionComponent* useBoolComponent4;
-		IBoolConditionComponent* notComponent2;
-		IFormConditionComponent* actorBaseComponent;
+		using DetectionCondition::DetectionCondition;
+
 	protected:
-		bool IsRelationship(RE::TESNPC* base, RE::TESNPC* target, RE::TESObjectREFR* a_refr) const;
-		bool FitsConditions(RE::Actor* actor, RE::Actor* target, RE::TESObjectREFR* a_refr) const;
-		bool ValidateTarget(RE::Actor* actor, RE::Actor* target) const;
 		bool EvaluateImpl(RE::TESObjectREFR* a_refr, RE::hkbClipGenerator* a_clipGenerator) const override;
 	};
 }
